e021bb07d253c92b10b9e65a62bc4dcafaea6997
Vigi <Vigi0303@163.com>
2016-05-08 23:38:34 +0800

    commit

--- douban filter.py
+++ douban filter.py
@@ -11,6 +11,34 @@ class Topic(object):
 		self.reply = reply
 		self.group = group
 
+class BlackFilter(object):
+	def __init__(self, file_name):
+		self.file_name = file_name
+		self.black_list = []
+		with open(self.file_name, "r", encoding="utf-8") as f:
+			for line in f.readlines():
+				line = line.strip()
+				if not len(line):
+					continue
+				self.black_list.append(line)
+
+	def contain(self, item):
+		return item in self.black_list
+
+	def append(self, item):
+		if item not in self.black_list:
+			self.black_list.append(item)
+
+	def save(self):
+		first_line = True
+		with open(self.file_name, "w", encoding="utf-8") as f:
+			for item in self.black_list:
+				if first_line:
+					first_line = False
+				else:
+					f.write("\n")
+				f.write(item)
+
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码
 
 cur_page_no = 1
@@ -26,13 +54,9 @@ req.add_header('Referer', group_url)
 req.add_header('Cookie', 'bid="tPUrC0sGWyU"; ll="108288"; _ga=GA1.2.273173056.1453032179; ps=y; ue="Vigi0303@163.com"; dbcl2="60652504:BGlb29Fu1J4"; ct=y; ck=s53H; ap=1; push_noty_num=0; push_doumail_num=0; _pk_ref.100001.8cb4=%5B%22%22%2C%22%22%2C1462631780%2C%22https%3A%2F%2Fwww.google.com%2F%22%5D; _pk_id.100001.8cb4=ccab1e82873542ba.1453124943.36.1462631780.1462615040.; _pk_ses.100001.8cb4=*; __utmt=1; __utma=30149280.273173056.1453032179.1462614717.1462631781.90; __utmb=30149280.2.10.1462631781; __utmc=30149280; __utmz=30149280.1462614717.89.50.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmv=30149280.6065')
 url_content = urllib.request.urlopen(req).read().decode("utf-8", "ignore")
 
-black_list = []
-with open("black_list.txt", "r", encoding="utf-8") as f:
-	for line in f.readlines():
-		line = line.strip()
-		if not len(line):
-			continue
-		black_list.append(line)
+url_filter = BlackFilter("black_list.txt")
+title_filter = BlackFilter("black_title_list.txt")
+
 url_list = []
 topic_list = []
 is_quit = False
@@ -47,7 +71,9 @@ for tr in table[0]:
 	reply = tr[1].text
 	group = tr[3][0].text
 	topic = Topic(topic_url, title, reply, group)
-	if topic_url in black_list:
+	if url_filter.contain(topic_url):
+		continue
+	if title_filter.contain(title):
 		continue
 	print("============================")
 	print(topic.title)
@@ -65,8 +91,8 @@ for tr in table[0]:
 			topic_list.append(topic)
 		print("saved-->",topic_url)
 	elif choose == "n":
-		if topic_url not in black_list:
-			black_list.append(topic_url)
+		url_filter.append(topic_url)
+		title_filter.append(title)
 		print("ignore-->",topic_url)
 	elif choose == "q":
 		is_quit = True
@@ -75,13 +101,7 @@ for tr in table[0]:
 		print("wrong input and next")
 	print("")
 
-first_line = True
-with open("black_list.txt", "w", encoding="utf-8") as f:
-	for black in black_list:
-		if first_line:
-			first_line = False
-		else:
-			f.write("\n")
-		f.write(black)
+url_filter.save()
+title_filter.save()
 
 print("end program")
